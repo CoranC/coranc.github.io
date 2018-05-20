5aedc0061d9f35e752cb341a2f86880cc9e23c7f
CoranC <coranc@gmail.com>
2018-05-20 17:43:57 +0100

    Update viz.js
    Added easing and yearly population

--- d3/irish-house-prices/viz.js
+++ d3/irish-house-prices/viz.js
@@ -1,5 +1,6 @@
-let width = 750,
+let width = 1000,
     height = 500;
+let buffer = 200;
 let year = 1995;
 
 let scaleX;
@@ -12,35 +13,42 @@ let svg = d3.select("body").append("svg")
   .attr("height", height);
 
 
-d3.json("./housePrices.json", (data)=>{
+d3.json("/housePrices.json", (err,data) =>{
+  if(err) console.log(`error fetching data:${err}`);
   houseData = data;
-  
+  console.log(data);
   scaleX = d3.scale.linear()
     .domain([1, data.length])
-    .range([50, width-100]);
+    .range([buffer, width-buffer]);
   scaleY = d3.scale.linear()
     .domain([35000, 595000])
     .range([0, 500-50].reverse());
   scalePopulation = d3.scale.linear()
-    .domain([85000, 1300000])
+    .domain([164200, 1345000])
     .range([10, 50]);
 
-  let yAxis = d3.svg.axis().scale(scaleY)
+  let yAxisL = d3.svg.axis().scale(scaleY)
     .orient("left");
-  let yAxisGroup = svg.append("g")
+  let yAxisR = d3.svg.axis().scale(scaleY)
+    .orient("right");
+  let yAxisGroupL = svg.append("g")
     .attr('class', 'axis')
-    .attr('transform', 'translate('+720+', '+0+')')
-    .call(yAxis);
+    .attr('transform', `translate(${buffer/2},0)`)
+    .call(yAxisL);
+  let yAxisGroupR = svg.append("g")
+    .attr('class', 'axis')
+    .attr('transform', `translate(${width - buffer/2}, 0)`)
+    .call(yAxisR);
   
   let x = d3.scale.ordinal()
     .domain(['Dublin', 'Cork', 'Galway', 'Limerick'])
-    .rangePoints([50, width-100]);
+    .rangePoints([buffer, width-buffer]);
   let xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
   let xAxisGroup = svg.append("g")
     .attr('class', 'axis')
-    .attr('transform', 'translate(0, '+470+')')
+    .attr('transform', 'translate(0, 480)')
     .call(xAxis);
   
   update(houseData, year)
@@ -48,24 +56,43 @@ d3.json("./housePrices.json", (data)=>{
 
 
 function update(data, year){
-  svg.selectAll("circle").remove();
   
   var circle = svg.selectAll("circle")
-    .data(data)
-    .enter()
+    .data(data);
+  
+  circle.exit().remove();
+  
+    circle
+    .transition()
+      .ease("easeElastic") 
+      .duration(1000)
+      .attr("cx", (d)=>{
+        return scaleX(+d.ID)
+      })
+      .attr("cy", (d)=>{
+        return scaleY(+d["price_" + year])
+      })
+      .attr("r", (d)=>{
+        return scalePopulation(+d["pop_" + year]);
+      })
+      .attr("fill", (d)=>{
+        return d.Color;
+      }) 
+    
+  circle.enter()
     .append("circle")
-    .attr("cx", (d)=>{
-      return scaleX(+d.ID)
-    })
-    .attr("cy", (d)=>{
-      return scaleY(+d[year])
-    })
-    .attr("r", (d)=>{
-      return scalePopulation(+d.Population);
-    })
-    .attr("fill", (d)=>{
-      return d.Color;
-    })
+      .attr("cx", (d)=>{
+        return scaleX(+d.ID)
+      })
+      .attr("cy", (d)=>{
+        return scaleY(+d["price_" + year])
+      })
+      .attr("r", (d)=>{
+        return scalePopulation(+d["pop_" + year]);
+      })
+      .attr("fill", (d)=>{
+        return d.Color;
+      });
 }
 
 
@@ -77,4 +104,4 @@ setInterval(()=>{
   document.getElementById("year").innerText = year;
 
   update(houseData, year);
-}, 1000);
+}, 500);
